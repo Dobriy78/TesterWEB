0b1fc12b7d6f40e427134ff489ef5420188309af
Dobriy78 <robe@bk.ru>
2013-05-15 17:51:42 +0400

    add new test

--- src/testerWEB/SomeTest.java
+++ src/testerWEB/SomeTest.java
@@ -1,18 +1,25 @@
 package testerWEB;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class SomeTest extends WebDriverAbstract {
 	
 	static Variable my = new Variable();
 	static String UrlTest = "http://www.ya.ru"; //my.getBaseUrl();
+	WebDriver driver = getDriver(); 
 
 	  @Test
 	  public void myTest() throws Exception {
-		WebDriver driver = getDriver();  
-		SeleniumCommands.Open(UrlTest, driver);
+ 		SeleniumCommands.Open(UrlTest, driver);
 		SeleniumCommands.Sleep(3000);
 	  }
-	
+	  
+	  @Test
+	  public void myTest2() throws Exception {
+		SeleniumCommands.TypeText(By.id("text"),"Hello!", driver);
+		SeleniumCommands.Click(By.xpath("//input[@value='']"), driver);
+		SeleniumCommands.Sleep(30000);
+	  }
 }
